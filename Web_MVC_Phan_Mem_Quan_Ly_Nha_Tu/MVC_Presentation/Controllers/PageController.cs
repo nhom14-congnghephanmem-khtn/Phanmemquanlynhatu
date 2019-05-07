6642bbd68253acc30a8d1ce8657a293540438af3
Adizsoft <adizsoft.com@gmail.com>
2019-05-07 13:59:23 +0700

    Màn Hình Hoạt Động Của Bộ Phận
    Tạo Màn Hình Hoạt Động Của Bộ Phận Sử Dụng BootStrap Version 1

--- Web_MVC_Phan_Mem_Quan_Ly_Nha_Tu/MVC_Presentation/Controllers/PageController.cs
+++ Web_MVC_Phan_Mem_Quan_Ly_Nha_Tu/MVC_Presentation/Controllers/PageController.cs
@@ -33,5 +33,9 @@ namespace MVC_Presentation.Controllers
         {
             return View();
         }
+        public ActionResult HoatDongCuaBoPhan()
+        {
+            return View();
+        }
     }
 }
